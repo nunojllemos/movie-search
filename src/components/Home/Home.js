592f77056cdc8bbae9ebea930a742a05a133ef1a
Nuno Lemos <nunojllemos@gmail.com>
2020-11-16 22:29:26 +0000

    Added fetch data function and first page load control using useRef

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import Search from "../Search/Search";
 import LandPage from "../LandPage/LandPage";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
-	return (
+	const [search, setSearch] = useState("");
+	const [moviesList, setMoviesList] = useState([]);
+	const firstLoad = useRef(true);
+
+	const handleSubmit = (search) => {
+		setSearch(search);
+		console.log(search);
+	};
+
+	const fetchData = (search) => {
+		const searchResult = axios
+			.get(`http://www.omdbapi.com/?apikey=443f62b0&s=${search}`)
+			.then((res) => {
+				if (res.data.Response === "True") {
+					// store results found in moviesList state
+					setMoviesList(res.data.Search);
+				} else {
+					// no results found
+					setMoviesList([]);
+				}
+			});
+
+		return searchResult;
+	};
+
+	useEffect(() => {
+		if (!firstLoad.current) {
+			fetchData(search);
+		} else {
+			firstLoad.current = false;
+		}
+	}, [search]);
+
+	// content
+	let content = (
 		<>
-			<Search />
-			<LandPage />
+			<Search handleSubmit={handleSubmit} />
+			{firstLoad.current ? <LandPage /> : <h1>MOVIE LIST</h1>}
 			<Footer />
 		</>
 	);
+
+	return content;
 };
 
 export default Home;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Search = (props) => {
+const Search = ({ handleSubmit }) => {
+	const [search, setSearch] = useState();
+
+	const handleInputChange = (e) => {
+		setSearch(e.target.value);
+	};
+
 	let content = (
-		<form onSubmit={() => {}}>
-			<input placeholder='Search...' className='input' type='text' />
+		<form
+			onSubmit={(e) => {
+				e.preventDefault();
+				handleSubmit(search);
+			}}>
+			<input
+				placeholder='Search...'
+				className='input'
+				type='text'
+				onChange={handleInputChange}
+			/>
 		</form>
 	);
 
